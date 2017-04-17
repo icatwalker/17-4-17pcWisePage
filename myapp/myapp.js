df1519298668a6b175cee5be3386c8c4abcb051f
icatwalker <m804396134@gmail.com>
2017-04-17 18:11:27 +0800

    wisePage

---
+++ myapp/myapp.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Administrator on 2017/4/17.
+ */
+var cw=document.documentElement.clientWidth;
+var ch=document.documentElement.clientHeight;
+var sw=screen.width;
+var sh=screen.height;
+console.log("窗口宽："+cw+"窗口高："+ch);
+console.log("屏幕宽："+sw+"屏幕高："+sh);
+
+//$("html").css("width","1920px");
+//console.log($("html").css("width"));
