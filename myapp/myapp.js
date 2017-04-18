4e3cfca9768c5b6db55b8bda1e960889ac53f349
icatwalker <m804396134@gmail.com>
2017-04-18 18:08:52 +0800

    17/4/18
    模型大体实现，差动态生成数据

--- myapp/myapp.js
+++ myapp/myapp.js
@@ -5,8 +5,12 @@ var cw=document.documentElement.clientWidth;
 var ch=document.documentElement.clientHeight;
 var sw=screen.width;
 var sh=screen.height;
+var iw=document.innerWidth;
+var ih=document.innerHeight;
+
 console.log("窗口宽："+cw+"窗口高："+ch);
 console.log("屏幕宽："+sw+"屏幕高："+sh);
+console.log("innerWidth："+iw+"innerHeight："+ih);
 
 //$("html").css("width","1920px");
 //console.log($("html").css("width"));
